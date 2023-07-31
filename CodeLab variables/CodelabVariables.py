c8aebd0ac72daf29b720db510d8450bd6852d942
Dylan Sotelo <dyalnalzat@gmail.com>
2023-07-31 18:17:18 -0500

    Nuevas respuestas

---
+++ CodeLab variables/CodelabVariables.py
@@ -0,0 +1,20 @@
+d = 35
+print(type(d))
+y = 2.34
+print(type(y))
+l = 4.567
+print(type("l"))
+k = True
+print(type(k))
+
+#Tipo entero: representan el valor de los numeros enteros.
+#Tipo float: representan los numeros decimales.
+#Tipo string: representa toda la cadena de texto.
+#Tipo booleano: representa los valores True or False
+
+name = "Precio electrodomesticos"
+n = 450000
+tv = 1000000
+nevera = 2000000
+print({name}, nevera+tv+n)
+
